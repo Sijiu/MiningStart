a8e9d957be72a092e100df59840bcdd5db62f9ec
Plain <mxh403@163.com>
2016-07-27 01:20:47 +0800

    python2 is fifferent from python3

--- Chapter1/affinity.py
+++ Chapter1/affinity.py
@@ -1,6 +1,8 @@
 # coding:utf-8
 import numpy as np
 from collections import defaultdict
+from operator import itemgetter
+
 dataset_filename = "affinity_dataset.txt"
 X = np.loadtxt(dataset_filename)
 n_samples, n_features = X.shape
@@ -29,13 +31,13 @@ for sample in X:  # sample 样本
                 valid_rules[(premise, conclusion)] += 1
             else:
                 invalid_rules[(premise, conclusion)] += 1
-print num_occurences
+# print num_occurences
 support = valid_rules
 confidence = defaultdict(float)
 for premise, conclusion in valid_rules.keys():
     rule = (premise, conclusion)
     confidence[rule] = valid_rules[rule] / num_occurences[premise]
-print confidence
+# print confidence
 
 def print_rule(premise, conclusion, support, confidence, features):
     premise_name = features[premise]
@@ -44,7 +46,12 @@ def print_rule(premise, conclusion, support, confidence, features):
     print "- Support : {0}".format(support[(premise, conclusion)])
     print "- Confidence : {0:.3f}".format(confidence[(premise, conclusion)])
 
-print_rule(1, 3, support, confidence, features)
+# print_rule(1, 3, support, confidence, features)
+sorted_support = sorted(support.items(), key=itemgetter(1), reverse=True)
+for index in range(5):
+    print "Rule #{0}".format(index + 1)
+    premise, conclusion = sorted_support[index][0]
+    print_rule(premise, conclusion, support, confidence, features)
 
 
 
